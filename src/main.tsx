3b68bcac678aca6ff5ca146cdb97c3d03b80069b
ohs <ohs6006@gmail.com>
2023-08-16 12:57:01 +0900

    에러 페이지 스타일링

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,13 @@ import "./styles/index.css";
 
 import Root from "./pages/Root";
 import Home from "./pages/Home";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <div>Error!</div>,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,3 +1,32 @@
-export default function Error() {
-  return <div>Error</div>;
+import styled from "styled-components";
+
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-family: Arial, sans-serif;
+`;
+
+const ErrorCode = styled.h1`
+  font-size: 6rem;
+  margin-bottom: 10px;
+  color: ${(props) => props.theme.colors.error};
+`;
+
+const ErrorMessage = styled.p`
+  font-size: 1.5rem;
+  text-align: center;
+`;
+
+function Error() {
+  return (
+    <ErrorContainer>
+      <ErrorCode>404</ErrorCode>
+      <ErrorMessage>Oops! The page you are looking for does not exist.</ErrorMessage>
+    </ErrorContainer>
+  );
 }
+
+export default Error;
